012bd6c645ca8c4b41e8b675fcddbaabdfc0fb94
Manuela Bognar <manuelabcamara@gmail.com>
2020-03-12 13:33:02 -0300

    Utilizando "state"

--- src/App.js
+++ src/App.js
@@ -1,49 +1,43 @@
-import React from 'react';
-import Tabela from './tabela.js'
+import React, { Component } from 'react';
+import Tabela from './Tabela.js'
 
 
+class App extends Component {
 
+  state = {
+    autores: [
+      {
+        nome: 'Shakespeare',
+        livro: 'Romeu e Julieta',
+        preco: '35'
+      },
+      {
+        nome: 'Machado de Assis',
+        livro: 'Dom Casmurro',
+        preco: '20'
+      },
+      {
+        nome: 'Kafka',
+        livro: 'A metamorfose',
+        preco: '25'
+      },
+      {
+        nome: 'Camus',
+        livro: 'O estrangeiro',
+        preco: '30'
+      }
+    ],
 
-
-
-
-function App() {
-
-  const  autores =  [
-    {
-      nome: 'Shakespeare',
-      livro: 'Romeu e Julieta',
-      preco: '35'
-    },
-    {
-      nome: 'Machado de Assis',
-      livro: 'Dom Casmurro',
-      preco: '20'
-    },
-    {
-      nome: 'Kafka',
-      livro: 'A metamorfose',
-      preco: '25'
-    },
-    {
-      nome: 'Camus',
-      livro: 'O estrangeiro',
-      preco: '30'
-    }
-  ];
-
-  return (
-    <div className="App">
-      <Tabela autores = { autores } />
-    </div>
-  );
-
+  }
+ 
+  render() {
+    return (
+      <div className="App">
+        <Tabela autores = { this.state.autores } />
+      </div>
+    );  
+  }
   
-  return (
-    <div className="App">
-      <Tabela />
-    </div>
-  );
 }
 
 export default App;
