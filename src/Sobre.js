296d1673494fe261064da95fc4b5fb98a131bf05
Manuela Bognar <manuelabcamara@gmail.com>
2020-03-17 18:39:26 -0300

    Criação das outras páginas (Routes)
    - Criacao das paǵinas: autores, livros, sobre, não encontrado, main
    - Rotas para todas elas utilizando react-router-dom
    - Utilizando componente para renderizar tabelas que tem basicamente a mesma estrutura

---
+++ src/Sobre.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Sobre = () => {
+  return (
+    <div className="container mt-10">
+      <h1>Sobre</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at rutrum diam, ac pellentesque ligula. Donec bibendum felis eu ipsum tempor, placerat tincidunt metus iaculis. Praesent vel quam eu ligula luctus auctor sed rutrum turpis. Proin libero purus, dapibus eu pretium et, vehicula vitae dolor. Aliquam erat volutpat. Sed convallis in nisi id consectetur. Mauris nec vehicula mauris. Sed molestie molestie ultricies. Nullam blandit facilisis metus id egestas. Pellentesque nec dolor at dolor consectetur rutrum accumsan nec lorem. 
+      </p>
+      <p>
+        <br></br>
+        Nunc ac tortor ut nibh elementum fringilla. Phasellus id auctor turpis. In finibus, mi ut ullamcorper luctus, ex dolor bibendum tellus, et elementum tellus velit a quam. Donec quis eros vitae sapien convallis rutrum in tincidunt sapien. Nulla facilisi. Proin egestas finibus vestibulum. Pellentesque vestibulum tortor turpis, non posuere turpis elementum non. Aenean efficitur eget orci quis tincidunt. Mauris sit amet porta ligula. Curabitur vitae nulla eget ex accumsan malesuada eu vel tellus. Aenean ac volutpat arcu.         
+      </p>
+      <p>
+        <br></br>
+        Sed pellentesque nulla tellus, nec euismod dui auctor sed. Phasellus consectetur semper nisl, in luctus urna fermentum nec. Sed non efficitur libero, eu vestibulum orci. Vivamus molestie facilisis mi, eget placerat mauris aliquet ut. Fusce vel neque at odio sagittis viverra. Pellentesque maximus purus mauris, ac malesuada turpis faucibus id. Nam lobortis et nisl a imperdiet. Pellentesque quis tellus sagittis, blandit nunc vitae, luctus ante. Nulla felis odio, congue quis felis sit amet, blandit rutrum tellus. Suspendisse elementum turpis tortor. Quisque suscipit ante vitae metus blandit, eu tincidunt erat tempus. 
+      </p>
+      <p>
+        <br></br>
+        Maecenas sed lacus quis enim facilisis egestas. Proin iaculis, arcu dapibus volutpat auctor, lorem augue laoreet massa, vel malesuada ligula urna non libero. Suspendisse potenti. Donec at purus condimentum, molestie nunc et, dictum nisl. Aenean augue risus, vulputate sit amet imperdiet eu, vehicula in augue. Integer dictum consectetur elit, ac varius quam rutrum vel. Ut eget libero consectetur, iaculis enim non, lobortis ante. Ut et iaculis lorem, varius efficitur nibh. Aenean ante diam, euismod sed maximus nec, molestie cursus felis. Phasellus eu porttitor tortor, id pharetra lectus. In id leo diam. Donec ultricies dignissim ornare. Pellentesque sodales auctor massa. 
+      </p>
+      <p><br></br><br></br></p>
+    </div>
+  );
+}
+
+export default Sobre;
